2229e62617dc5905a5dbe806599d58820a492871
Pratik Panchani <61979041+pratik53@users.noreply.github.com>
2021-05-20 20:26:06 -0400

    Stacks

--- src/com/pratik/Main.java
+++ src/com/pratik/Main.java
@@ -1,14 +1,12 @@
 package com.pratik;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-	    LinkedList list = new LinkedList();
-	    list.addLast(10);
-        list.addLast(20);
-        list.addLast(30);
-        System.out.println(list.getKthFromTheEnd(0));
+	    Stack stack = new Stack();
+	    stack.push(10);
+		stack.push(20);
+		stack.push(30);
+		System.out.println(stack.peek());
     }
 }
